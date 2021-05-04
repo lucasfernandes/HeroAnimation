c156115759d88de81d64f3c220996dcac50860d2
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2021-05-04 14:54:20 -0300

    Initial Commit

---
+++ HeroAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeroAnimation
+//
+//  Created by Lucas Silveira on 04/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HeroAnimation/HeroAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroAnimationApp.swift
+//  HeroAnimation
+//
+//  Created by Lucas Silveira on 04/05/21.
+//
+
+import SwiftUI
+
+@main
+struct HeroAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
